c24b9f2e8e0b3c04f06ecc498a8ec4feacc86c07
SoumodeepDas2004 <higherdas09@gmail.com>
2026-04-08 18:46:37 +0530

    updated dep ver newsmap

--- src/components/NewsMap.jsx
+++ src/components/NewsMap.jsx
@@ -1,4 +1,17 @@
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function NewsMap({ articles }) {
 
